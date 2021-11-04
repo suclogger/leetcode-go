0a870dbd1a44e30e8363dda2d60d16883230acd0
pr0found <runqiang.crq@alibaba-inc.com>
2021-11-05 00:08:58 +0800

    write by myself

--- chapter_13/merge-two-sorted-lists.go
+++ chapter_13/merge-two-sorted-lists.go
@@ -9,7 +9,7 @@ package main
  */
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
@@ -17,20 +17,20 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	res := &ListNode{}
 	cur := res
 	for l1 != nil && l2 != nil {
-        if l1.Val < l2.Val {
-            cur.Next = l1
-            l1 = l1.Next
-        } else {
-            cur.Next = l2
-            l2 = l2.Next
-        }
-        cur = cur.Next
-    }
+		if l1.Val < l2.Val {
+			cur.Next = l1
+			l1 = l1.Next
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
 	if l1 != nil {
-        cur.Next = l1
-    }
+		cur.Next = l1
+	}
 	if l2 != nil {
-        cur.Next = l2
-    }
+		cur.Next = l2
+	}
 	return res.Next
 }
