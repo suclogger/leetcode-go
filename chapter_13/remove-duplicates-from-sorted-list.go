9bd67cd06bcc6149382d051bae618f8a00a9e176
pr0found <runqiang.crq@alibaba-inc.com>
2021-11-24 23:10:04 +0800

    exe 1

---
+++ chapter_13/remove-duplicates-from-sorted-list.go
@@ -0,0 +1,16 @@
+package main
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
