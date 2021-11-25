d2bad85a3bca7c15daed72c21e38b1d847d08cff
pr0found <runqiang.crq@alibaba-inc.com>
2021-11-26 00:25:03 +0800

    快慢指针，边界情况要考虑

--- chapter_13/remove-nth-node-from-end-of-list.go
+++ chapter_13/remove-nth-node-from-end-of-list.go
@@ -1,1 +1,18 @@
 package main
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	fast, slow := head, head
+	for n > 0 && fast != nil {
+		fast = fast.Next
+		n--
+	}
+	if fast == nil {
+		return head.Next
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return head
+}
